2ce9175a8d35c92c0fc924a63b8ee83659166790
gvprasad <gvprasad.tav@gmail.com>
2018-05-08 02:17:35 +0000

    newly added 3rd line file

--- LoginPage.py
+++ LoginPage.py
@@ -18,6 +18,7 @@ class LoginPage(PageObject):
     def login_to_emc(self, username, password):
         """Login to modified """
         """ added extra line for comment"""
+        """"new line for comment3"""
         # Object
         login_btn = self.browser.find_element_by_css_selector("button[type='%s']" % self.locator.submit_button)
 
